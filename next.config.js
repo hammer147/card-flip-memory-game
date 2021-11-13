b5fb0f43555dbc5d61ffb6c4bc6a3c8256f5686d
Ben Hamerlynck <ben.hamerlynck@telenet.be>
2021-11-13 14:39:03 +0100

    fix bad-precaching-response

--- next.config.js
+++ next.config.js
@@ -1,10 +1,15 @@
 /** @type {import('next').NextConfig} */
 
 const withPWA = require('next-pwa')
+const runtimeCaching = require('next-pwa/cache')
 
 module.exports = withPWA({
   reactStrictMode: true,
   pwa: {
-    dest: 'public'
+    dest: 'public',
+    runtimeCaching,
+    buildExcludes: [/middleware-manifest\.json$/]
   }
 })
+
+// https://github.com/shadowwalker/next-pwa/issues/288#issuecomment-955777098
